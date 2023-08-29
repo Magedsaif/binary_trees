cfc528fa70b55def55fced760de6bfc80a7165a1
Magedsaif <magedsaif21@gmail.com>
2023-08-29 23:19:24 +0200

    edit betty for task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -14,6 +14,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	binary_tree_is_leaf(node->left);
 	binary_tree_is_leaf(node->right);
 	if (node->left == NULL && node->right == NULL)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
