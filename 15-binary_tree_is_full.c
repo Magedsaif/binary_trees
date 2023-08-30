50ed17ff24d0b709ddd5615b49630786c4057a2e
Magedsaif <magedsaif21@gmail.com>
2023-08-30 21:57:03 +0200

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,6 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left_balance, right_balance;
 
 	if (tree == NULL)
 		return (0);
@@ -19,5 +18,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
-		return (0);
+	return (0);
 }
