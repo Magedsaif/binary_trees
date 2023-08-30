a9c477e9aba1790c1d616764634bf5096e9a8f55
Magedsaif <magedsaif21@gmail.com>
2023-08-30 12:35:14 +0200

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-    return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
