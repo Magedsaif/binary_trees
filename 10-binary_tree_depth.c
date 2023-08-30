88a5f8ba77e5fb16687fb0f2b82fc8cdabeea730
Magedsaif <magedsaif21@gmail.com>
2023-08-30 13:03:45 +0200

    task 9

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,12 +7,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth;
 
-	if (tree == NULL || tree->parent == NULL)
-		return (0);
-
-	depth = binary_tree_depth(tree->parent) + 1;
-
-	return (depth);
 }
